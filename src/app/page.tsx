673eb30501849218b4a63908ada81fc1477fbebe
Ricardo Trevino <Rickytrevino2380@gmail.com>
2024-06-26 19:05:01 -0700

    added image linking and the like

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import 'tailwindcss/tailwind.css'
 import { PropsWithChildren } from 'react';
 import Sidebar from '@/app/sidebar'
 import {montserrat} from '@/app/layout'
+import Image from 'next/image';
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
@@ -11,6 +12,14 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         <main>
         <Sidebar />
           <h1 className="text-center text-color:blue ">My name is Ricardo and this is my website! welcome!</h1>
+
+          <Image
+        src="/beach.jpg"
+        width={700}
+        height={400}
+        className=" md:block"
+        alt=""
+      />
 
          </main>
       </body>
